609da88f37b2ff085d21558bb4dc805236cc4cda
masud001 <madhnagar@gmail.com>
2020-08-24 00:48:59 +0600

    markup done up to header

--- sabbirAhmed06/project/assets/js/main.js
+++ sabbirAhmed06/project/assets/js/main.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
+	// When your page loads
+	$(function () {
+		// When the toggle areas in your navbar are clicked, toggle them
+		$("#search-button, #search-icon").click(function (e) {
+			e.preventDefault();
+			$("#search-button, #search-form").toggle();
+		});
+	});
+
 	// this carousel for about us section
 	// ==================================
 	var owl = $(".about-carousel");
